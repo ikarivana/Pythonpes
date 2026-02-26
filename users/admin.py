e7f52fec8d4e1ee14f9dbeae11522f67a512e89a
ikarivana <ikrajscakova@gmail.com>
2026-02-26 23:33:55 +0100

    simple

--- users/admin.py
+++ users/admin.py
@@ -28,32 +28,6 @@ class PesAdmin(admin.ModelAdmin):
     search_fields = ('jmeno', 'cip', 'majitel__uzivatel__username')
 
 
-# --- PROFIL MAJITELE ---
-@admin.register(models.ProfilMajitele)
-class ProfilMajiteleAdmin(admin.ModelAdmin):
-    # Přidali jsme adresu a použitý promo kód do přehledu
-    list_display = ('uzivatel', 'is_premium', 'premium_do', 'pouzity_kod', 'mesto', 'telefon')
-    # Umožníme měnit premium status a adresu přímo v tabulce pro rychlou opravu
-    list_editable = ('is_premium', 'premium_do')
-    # Filtrování, abys rychle našla lidi z konkrétního útulku nebo výstavy
-    list_filter = ('is_premium', 'pouzity_kod', 'mesto')
-    search_fields = ('uzivatel__username', 'uzivatel__email', 'pouzity_kod', 'mesto')
-
-    # Organizace polí v detailu profilu
-    fieldsets = (
-        ('Základní údaje', {
-            'fields': ('uzivatel', 'telefon')
-        }),
-        ('Doručovací adresa pro dárky', {
-            'fields': ('ulice_cp', 'mesto', 'psc')
-        }),
-        ('Premium a Marketing', {
-            'fields': ('is_premium', 'premium_do', 'pouzity_kod'),
-            'description': 'Zde vidíte, zda uživatel získal premium přes promo kód.'
-        }),
-    )
-
-
 # --- PROMO KÓDY ---
 @admin.register(models.PromoKod)
 class PromoKodAdmin(admin.ModelAdmin):
