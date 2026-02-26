1f69deb84f1a270920f15433a1536e71f0533068
ikarivana <ikrajscakova@gmail.com>
2026-02-26 22:54:39 +0100

    simple

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import Sluzba, KontaktniZprava, ProfilMajitele # Všechny importy z jednoho místa
+from .models import Sluzba, KontaktniZprava
+from users.models import ProfilMajitele
 
 @admin.register(Sluzba)
 class SluzbaAdmin(admin.ModelAdmin):
